6e0abeb6e57161ee727c73714e4fe51d1414d93e
Moses Seloma <mseloma@gmail.com>
2019-02-02 20:30:58 +0200

    Attempt to remove node_modules from repository

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './containers/App';
+
+ReactDOM.render(<div><App title="Bible" author="God"/><App title="Magazine" author="Women"/></div>, document.getElementById('app'))
