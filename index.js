dfeeb88bdefad0c191ab79b9cebf702fcc3970a0
Moses Seloma <mseloma@gmail.com>
2019-02-02 21:08:02 +0200

    created new react project with node

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// main app
-import App from './containers/App';
-
-ReactDOM.render(<div><App title="Bible" author="God"/><App title="Magazine" author="Women"/></div>, document.getElementById('app'))
--- source/app.js
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Hello = React.createClass({
- render: function () {
- return <div>Hello {this.props.name}</div>;
- }
-});
